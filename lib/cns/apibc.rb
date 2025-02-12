1c325e7e63281c4208d61f1482aecdc9fdeff37f
Hernani Rodrigues Vaz <hernanirvaz@gmail.com>
2025-02-12 06:07:23 +0000

    cleanup

--- lib/cns/apibc.rb
+++ lib/cns/apibc.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
-require("faraday")
-require("json")
+require('faraday')
+require('json')
 
 # @author Hernani Rodrigues Vaz
 module Cns
@@ -11,15 +11,15 @@ module Cns
     # @param addresses [Array<String>] List of ETH addresses (max 20)
     # @return [Array<Hash>] List of addresses with balances
     def account_es(addresses)
-      response = etherscan_req("balancemulti", addresses.join(","), 1, tag: "latest")
-      response[:status] == "1" ? response.dig(:result) : []
+      response = etherscan_req('balancemulti', addresses.join(','), 1, tag: 'latest')
+      response[:status] == '1' ? response.dig(:result) : []
     end
 
     # Get EOS account information
     # @param address [String] EOS account name
     # @return [Hash] Account details with resources
     def account_gm(address)
-      response = greymass_req("/v1/chain/get_account", { account_name: address })
+      response = greymass_req('/v1/chain/get_account', { account_name: address })
       response || { core_liquid_balance: 0, total_resources: { net_weight: 0, cpu_weight: 0 } }
     end
 
@@ -27,35 +27,35 @@ module Cns
     # @param [String] address endereco ETH
     # @return [Array<Hash>] lista transacoes normais etherscan
     def norml_es(address)
-      pag_etherscan_req("txlist", address)
+      pag_etherscan_req('txlist', address)
     end
 
     # Get internal transactions for ETH address
     # @param (see norml_es)
     # @return [Array<Hash>] lista transacoes internas etherscan
     def inter_es(address)
-      pag_etherscan_req("txlistinternal", address)
+      pag_etherscan_req('txlistinternal', address)
     end
 
     # Get mined blocks for ETH address
     # @param (see norml_es)
     # @return [Array<Hash>] lista blocos etherscan
     def block_es(address)
-      pag_etherscan_req("getminedblocks", address, blocktype: "blocks")
+      pag_etherscan_req('getminedblocks', address, blocktype: 'blocks')
     end
 
     # Get withdrawals for ETH address
     # @param (see norml_es)
     # @return [Array<Hash>] lista blocos etherscan
     def withw_es(address)
-      pag_etherscan_req("txsBeaconWithdrawal", address)
+      pag_etherscan_req('txsBeaconWithdrawal', address)
     end
 
     # Get token transfers for ETH address
     # @param (see norml_es)
     # @return [Array<Hash>] lista token transfer events etherscan
     def token_es(address)
-      pag_etherscan_req("tokentx", address)
+      pag_etherscan_req('tokentx', address)
     end
 
     # Get complete transaction history for EOS account
@@ -65,7 +65,7 @@ module Cns
       actions = []
       pos = 0
       loop do
-        response = greymass_req("/v1/history/get_actions", { account_name: address, pos: pos, offset: 100 })
+        response = greymass_req('/v1/history/get_actions', { account_name: address, pos: pos, offset: 100 })
         batch = response[:actions] || []
         actions += batch
         break if batch.size < 100
@@ -80,23 +80,27 @@ module Cns
     # Reusable Faraday connection
     def connection(base_url)
       Faraday.new(base_url) do |conn|
-        conn.headers = { content_type: "application/json", accept: "application/json", user_agent: "blockchain-api-client" }
-        conn.adapter Faraday.default_adapter
+        conn.headers = {
+          content_type: 'application/json',
+          accept: 'application/json',
+          user_agent: 'blockchain-api-client'
+        }
+        conn.adapter(Faraday.default_adapter)
       end
     end
 
     # Generic Etherscan API request handler
     def etherscan_req(action, address, page = 1, params = {})
       params = {
-        module: "account",
+        module: 'account',
         action: action,
         address: address,
         page: page,
-        apikey: ENV.fetch("ETHERSCAN_API_KEY"),
+        apikey: ENV.fetch('ETHERSCAN_API_KEY')
       }.merge(params)
-      parse_json(connection("https://api.etherscan.io").get("/api", params).body)
+      parse_json(connection('https://api.etherscan.io').get('/api', params).body)
     rescue Faraday::Error, JSON::ParserError
-      { status: "0", result: [] }
+      { status: '0', result: [] }
     end
 
     # Generic method for paginated Etherscan requests
@@ -109,11 +113,11 @@ module Cns
       page = 1
       loop do
         response = etherscan_req(action, address, page, params)
-        break unless response[:status] == "1"
+        break unless response[:status] == '1'
 
         batch = response[:result] || []
         results += batch
-        break if batch.size < 10000
+        break if batch.size < 10_000
 
         page += 1
       end
@@ -122,7 +126,7 @@ module Cns
 
     # Generic Greymass API request handler
     def greymass_req(endpoint, payload)
-      parse_json((connection("https://eos.greymass.com").post(endpoint) { |req| req.body = payload.to_json }).body)
+      parse_json((connection('https://eos.greymass.com').post(endpoint) { |req| req.body = payload.to_json }).body)
     rescue Faraday::Error, JSON::ParserError
       nil
     end
